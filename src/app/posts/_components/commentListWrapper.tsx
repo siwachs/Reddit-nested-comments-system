5ed0da18f6371c9c2b9730642154d15174211cee
siwachs <ssiwach.8888@gmail.com>
2024-05-21 10:53:43 +0530

    Render Nested Comments

--- src/app/posts/_components/commentListWrapper.tsx
+++ src/app/posts/_components/commentListWrapper.tsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { FaEdit, FaHeart, FaReply, FaTrash } from "react-icons/fa";
+import IconButton from "./iconbutton";
+
 const CommentListWrapper: React.FC<{ comments: CommentObj[] }> = ({
   comments,
 }) => {
@@ -22,23 +26,85 @@ const CommentListWrapper: React.FC<{ comments: CommentObj[] }> = ({
     <section>
       {rootComments && rootComments.length && (
         <div className="mt-4">
-          <CommentList comments={rootComments} />
+          <CommentList comments={rootComments} getReplies={getReplies} />
         </div>
       )}
     </section>
   );
 };
 
-const CommentList: React.FC<{ comments: CommentObj[] }> = ({ comments }) => {
+const CommentList: React.FC<{
+  comments: CommentObj[];
+  getReplies: (parentId: string) => CommentObj[] | undefined;
+}> = ({ comments, getReplies }) => {
   return comments.map((comment) => (
     <div key={comment.id} className="comment-stack">
-      <Comment comment={comment} />
+      <Comment comment={comment} getReplies={getReplies} />
     </div>
   ));
 };
 
-const Comment: React.FC<{ comment: CommentObj }> = ({ comment }) => {
-  return <div>{comment.message}</div>;
+const dateFormatter = new Intl.DateTimeFormat(undefined, {
+  dateStyle: "medium",
+  timeStyle: "short",
+});
+
+const Comment: React.FC<{
+  comment: CommentObj;
+  getReplies: (parentId: string) => CommentObj[] | undefined;
+}> = ({ comment, getReplies }) => {
+  const [areChildrenHidden, setAreChildrenHidden] = useState(true);
+  const childComments = getReplies(comment.id);
+
+  return (
+    <>
+      <div className="comment">
+        <div className="header">
+          <span className="name">{comment.user.name}</span>
+          <span className="date">
+            {dateFormatter.format(Date.parse(comment.createdAt))}
+          </span>
+        </div>
+
+        <div className="message">{comment.message}</div>
+
+        <div className="footer">
+          <IconButton Icon={<FaHeart />} aria-label="Like">
+            2
+          </IconButton>
+          <IconButton Icon={<FaReply />} aria-label="Reply" />
+          <IconButton Icon={<FaEdit />} aria-label="Edit" />
+          <IconButton Icon={<FaTrash />} aria-label="Delete" color="danger" />
+        </div>
+      </div>
+
+      {childComments && childComments.length && (
+        <>
+          <div
+            className={`nested-comments-stack ${
+              areChildrenHidden ? "hide" : ""
+            }`}
+          >
+            <button
+              onClick={() => setAreChildrenHidden(true)}
+              className="collapse-line"
+              aria-label="Hide Replies"
+            />
+            <div className="nested-comments">
+              <CommentList comments={childComments} getReplies={getReplies} />
+            </div>
+          </div>
+
+          <button
+            onClick={() => setAreChildrenHidden(false)}
+            className={`btn mt-1 ${!areChildrenHidden ? "hide" : ""}`}
+          >
+            Show Replies
+          </button>
+        </>
+      )}
+    </>
+  );
 };
 
 export default CommentListWrapper;
